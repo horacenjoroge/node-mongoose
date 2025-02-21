291a261a4feced71f822368c6497b91e876e5ffc
“horacenjoroge” <horacenjorge@gmail.com>
2025-02-22 02:11:50 +0300

    Mongoose Part 2

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 const Campsite = require('./models/campsite');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
 connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
